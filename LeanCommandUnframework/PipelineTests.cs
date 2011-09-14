c218d8167d93416b1d4b294dd4bae6ac24ca805a
Szymon Pobiega <szymon.pobiega@gmail.com>
2011-09-15 00:30:33 +0200

    First tests

---
+++ LeanCommandUnframework/PipelineTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+// ReSharper disable InconsistentNaming
+namespace LeanCommandUnframework
+{
+    [TestFixture]
+    public class PipelineTests
+    {
+        [Test]
+        public void It_executes_the_handler()
+        {
+            var pipeline = new Pipeline(new HandlerCollection(typeof(TestCommandHandler)));
+
+            var result = pipeline.Process(new TestCommand());
+
+            Assert.IsInstanceOf<TestCommandResult>(result);
+        }
+    }
+}
+// ReSharper restore InconsistentNaming
